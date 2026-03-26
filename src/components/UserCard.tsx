4667d6553082cd0f856ecea56db8e036fff400de
Monedita <gonzalo_fder@hotmail.com.ar>
2026-03-26 17:11:04 +0100

    Add more information in UserCard.tsx. Small class styles fixed in SearchInput.tsx. Big improvement in useFilter.ts generating diferents regex from user search text input.

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -4,14 +4,18 @@ import CircularLoadingBar from './CircularLoadingBar';
 
 export default function UserCard({ user }: { user: User }) {
     return (
-        <li className="mb-4 rounded-full flex items-center gap-4 bg-gray-800 p-1">
+        <li className="mb-4 rounded-full flex items-center gap-4 bg-gray-800 p-1 text-gray-500">
             <div className='w-16 h-16 flex-none relative'>
                 <img src={user.img} alt={user.fullName} className="w-full h-full flex-none rounded-full" />
                 <CircularLoadingBar max={17} value={user.searchScore || 0} strokeWidth={4} />
             </div>
-            <div className="flex-1">
-                <h3 className="font-bold">{user.fullName}</h3>
-                <p className="text-sm text-gray-500">{user.email}</p>
+            <div className="flex-none shrink">
+                <h3 className="font-bold text-white">{user.fullName}</h3>
+                <p className="text-sm">{user.email}</p>
+            </div>
+            <div className="flex-1 shrink">
+                <p className="text-sm">{user.jobArea} [{user.jobType}] </p>
+                <p className="text-sm">{user.bio}</p>
             </div>
         </li>
     );
--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -4,7 +4,66 @@ import type { FilterKey } from '../types/FilterKey';
 
 type WithScore<T> = T & { searchScore?: number };
 
+interface regexCache {
+  regex: RegExp;
+  multiplier: number;
+}
+
 export default function useFilter<T>(items: T[], searchTerm: string, keys: FilterKey<T>[]): WithScore<T>[] {
+
+  const regexs = useMemo(() => {
+    const normalizedTerm = searchTerm.toLowerCase().trim();
+    const patterns: regexCache[] = [];
+    //user regex pattern
+    try {
+      patterns.push({ regex: new RegExp(normalizedTerm, 'i'), multiplier: 1 });
+    } catch (e) {
+    }
+    //transposition regex generation
+    for (let i = 0; i < normalizedTerm.length - 1; i++) {
+      try {
+        patterns.push(
+          {
+            regex: new RegExp(
+              `${normalizedTerm.slice(0, i)}${normalizedTerm[i + 1]}${normalizedTerm[i]}${normalizedTerm.slice(i + 2)}`, 'i'
+            ),
+            multiplier: 0.8
+          }
+        );
+      } catch (e) {
+      }
+    }
+    //typo regex generation
+    for (let i = 0; i < normalizedTerm.length; i++) {
+      try {
+        patterns.push(
+          {
+            regex: new RegExp(
+              `${normalizedTerm.slice(0, i)}.${normalizedTerm.slice(i + 1)}`, 'i'
+            ),
+            multiplier: 0.6
+          }
+        );
+      } catch (e) {
+      }
+    }
+    //insertion regex generation
+    for (let i = 0; i <= normalizedTerm.length; i++) {
+      try {
+        patterns.push(
+          {
+            regex: new RegExp(
+              `${normalizedTerm.slice(0, i)}${normalizedTerm.slice(i + 1)}`, 'i'
+            ),
+            multiplier: 0.5
+          }
+        );
+      } catch (e) {
+      }
+    }
+    return patterns;
+  }, [searchTerm]);
+
   return useMemo(() => {
     const normalizedTerm = searchTerm.toLowerCase().trim();
     if (!normalizedTerm) return items as WithScore<T>[];
@@ -15,6 +74,13 @@ export default function useFilter<T>(items: T[], searchTerm: string, keys: Filte
           const value = String(item[key.key]).toLowerCase();
           if (value.includes(normalizedTerm)) {
             score += key.weight;
+          } else {
+            for (const regex of regexs) {
+              if (regex.regex.test(value)) {
+                score += key.weight * regex.multiplier;
+                break;
+              }
+            }
           }
         });
         return { ...item, searchScore: score };
